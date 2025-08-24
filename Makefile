a0e81035a223f733234c44776f9fb20e9307d64a
Egor Zimin <ziminson@yandex.ru>
2025-08-24 19:08:19 +0300

    feat: MessageQueue implementation

--- Makefile
+++ Makefile
@@ -2,14 +2,21 @@ CXX := g++
 CXXFLAGS := -std=c++17 -Wall -Wextra -pedantic -fPIC
 LDFLAGS := -shared
 TARGET_LIB := libJournalLogger.so
+TARGET_APP := JournalLogger
 
-.PHONY: all clean
+.PHONY: all clean run
 
-all: $(TARGET_LIB)
+all: $(TARGET_LIB) $(TARGET_APP)
 
 $(TARGET_LIB): JournalLogger.cpp JournalLogger.h
 	$(CXX) $(CXXFLAGS) -c JournalLogger.cpp -o JournalLogger.o
 	$(CXX) $(LDFLAGS) -o $@ JournalLogger.o
 
+$(TARGET_APP): main.cpp $(TARGET_LIB)
+	$(CXX) $(CXXFLAGS) -L. -o $@ main.cpp -lJournalLogger -pthread
+
 clean:
-	rm -f $(TARGET_LIB) *.o
+	rm -f $(TARGET_LIB) $(TARGET_APP) *.o
+
+run: all
+	LD_LIBRARY_PATH=. ./$(TARGET_APP) test.log INFO
--- main.cpp
+++ main.cpp
@@ -1,9 +1,55 @@
+#include "JournalLogger.h"
 #include <iostream>
+#include <thread>
+#include <queue>
+#include <condition_variable>
+#include <atomic>
+#include <memory>
 
-using namespace std;
+class MessageQueue {
+public:
+    void push(const std::string& message, ImportanceLevel level) {
+        std::unique_lock<std::mutex> lock(mutex);
 
-int main()
-{
-    cout << "Hello World!" << endl;
+        queue.push({message, level});
+
+        lock.unlock();
+
+        condition.notify_one();
+    }
+
+    std::pair<std::string, ImportanceLevel> pop() {
+        std::unique_lock<std::mutex> lock(mutex);
+
+        condition.wait(lock, [this] {
+            return !queue.empty() || stopped;
+        });
+
+        if (stopped && queue.empty()) {
+            return{"", ImportanceLevel::Medium};
+        }
+
+        auto item = queue.front();
+        queue.pop();
+        return item;
+    }
+
+
+    void stop() {
+        std::unique_lock<std::mutex> lock(mutex);
+        stopped = true;
+
+        condition.notify_all();
+    }
+
+
+private:
+    std::queue<std::pair<std::string, ImportanceLevel>> queue;
+    std::mutex mutex;
+    std::condition_variable condition;
+    std::atomic<bool> stopped{false};
+};
+
+int main(int argc, char* argv[]) {
     return 0;
 }
